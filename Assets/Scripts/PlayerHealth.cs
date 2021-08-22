95e40bcc37aecb34878a333335d07bee9c17dd46
Andy <81387021+snailythefan@users.noreply.github.com>
2021-08-22 15:37:58 -0500

    Se implemento sistema de vidas
    jugador tiene 3 vidas

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -13,6 +13,12 @@ public class PlayerHealth : MonoBehaviour {
 	public GameObject mitti;
 	public GameObject Bgmusic;
 
+	public GameObject Life1;
+	public GameObject Life2;
+	public GameObject Life3;
+
+	public AudioSource hurtNoise;
+
 	void Start(){
 		health = maxHealth;
 	}
@@ -25,12 +31,29 @@ public class PlayerHealth : MonoBehaviour {
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision){
-		if(collision.CompareTag("Enemy") && !isInmune){
+		if(collision.CompareTag("Enemy") && !isInmune)
+		{
 			health -= collision.GetComponent<Enemy> ().damageToGive;
 			StartCoroutine (Inmunity ());
-			if (health <= 0) {
+
+			if (health == 2)
+			{
+				Life3.SetActive(false);
+				hurtNoise.Play();
+			}
+			else if (health == 1)
+			{
+				Life2.SetActive(false);
+				hurtNoise.Play();
+
+			}
+
+			else if (health == 0)
+			{
 
 				//aparecer pantalla de game over
+				Life1.SetActive(false);
+				hurtNoise.Play();
 				pantallaLose.SetActive(true);
 				Destroy(mitti);
 				Destroy(Bgmusic);

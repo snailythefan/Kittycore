8158b275d1d8eb690ff444906b6cc89a90b45b25
Micaela Acosta <micaelaac03@gmail.com>
2021-08-22 04:28:26 -0300

    Implementación de trampolín
    correcciones en disparos

---
+++ Assets/Scripts/LifeTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeTime : MonoBehaviour {
+
+	[SerializeField] private float tiempoDeVida;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, tiempoDeVida);
+	}
+}
---
+++ Assets/Scripts/Trampolin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampolin : MonoBehaviour {
+
+	public float jumpForce = 2f;
+
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		collision.gameObject.GetComponent<Rigidbody2D>().velocity = (Vector2.up * jumpForce);
+	}
+
+}

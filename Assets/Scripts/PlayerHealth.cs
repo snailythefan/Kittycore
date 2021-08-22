97df2ceeed341f6dd006c7b7d8444f157e53eb14
Micaela Acosta <micaelaac03@gmail.com>
2021-08-21 22:12:25 -0300

    integracion de enemigo en TestNivel2

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public float health;
+	public float maxHealth;
+	bool isInmune;
+	public float inmunityTime;
+
+	void Start(){
+		health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health > maxHealth) {
+			health = maxHealth;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision){
+		if(collision.CompareTag("Enemy") && !isInmune){
+			health -= collision.GetComponent<Enemy> ().damageToGive;
+			StartCoroutine (Inmunity ());
+			if (health <= 0) {
+
+				//aparecer pantalla de game over
+				print ("player dead");
+			}
+		}
+
+	}
+
+	IEnumerator Inmunity(){
+		isInmune = true;
+		yield return new WaitForSeconds (inmunityTime);
+		isInmune = false;
+	}
+}

abb7df10ed64d70c9df51393df3bec170838b75e
Andy <81387021+snailythefan@users.noreply.github.com>
2021-08-22 12:59:33 -0500

    Animacion enemigo
    Bat

---
+++ Assets/Scripts/AvanceIntro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvanceIntro : MonoBehaviour {
+
+	public GameObject pantalla1;
+	public GameObject pantalla2;
+
+	public AudioSource click;
+
+	public void Avance()
+	{
+		click.Play();
+		pantalla1.SetActive(false);
+		pantalla2.SetActive(true);
+	}
+
+
+
+}

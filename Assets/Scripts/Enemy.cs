117fb1214119285f96f9f00adee76d48bfc55d59
Micaela Acosta <micaelaac03@gmail.com>
2021-08-21 21:24:19 -0300

    Subo el movimiento del enemigo

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public string enemyName;
+	public float healthPoints;
+	public float speed;
+	public float knockbackForceX;
+	public float knockbackForceY;
+	public float damageToGive;
+
+
+}
---
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoEnemigo : MonoBehaviour {
+
+	public float DistancePlayer;
+	public float RangeVision;
+	public float RangeReverse;
+	public float Speed;
+	public float ReverseSpeed;
+
+	public Transform Player;
+	public Rigidbody2D Rb2d;
+
+	private void Update(){
+		DistancePlayer = Vector2.Distance (Player.position, Rb2d.position);
+		if (DistancePlayer < RangeVision && DistancePlayer > RangeReverse) {
+			Vector2 Objetivo = new Vector2 (Player.position.x, Player.position.y); 
+			Vector2 NuevaPosicion = Vector2.MoveTowards (Rb2d.position, Objetivo, Speed * Time.deltaTime);
+			Rb2d.MovePosition (NuevaPosicion);
+		} else if (DistancePlayer < RangeReverse) {
+			Vector2 Objetivo = new Vector2 (Player.position.x, Player.position.y); 
+			Vector2 NuevaPosicion = Vector2.MoveTowards (Rb2d.position, Objetivo, ReverseSpeed * Time.deltaTime);
+			Rb2d.MovePosition (NuevaPosicion);
+		}
+	}
+}
